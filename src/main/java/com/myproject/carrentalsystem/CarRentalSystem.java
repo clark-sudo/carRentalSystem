45b292250495a6d571abcdc56858f5c1d0125bf6
clark-sudo <clarksonlimuco29@gmail.com>
2026-04-13 19:28:30 +0800

    UI - Limuco

--- src/main/java/com/myproject/carrentalsystem/CarRentalSystem.java
+++ src/main/java/com/myproject/carrentalsystem/CarRentalSystem.java
@@ -4,13 +4,21 @@
 
 package com.myproject.carrentalsystem;
 
+import javax.swing.*;
+
 /**
  *
  * @author hicru
  */
 public class CarRentalSystem {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String choice = (String) JOptionPane.showInputDialog(null, "Select Screen Size", "Screen selection", 
+                JOptionPane.QUESTION_MESSAGE, null, homePage.screenSizes.toArray(), homePage.screenSizes.get(2));
+        
+        if (choice == null) choice = "Normal Screen";
+        
+        loginPage lp = new loginPage(choice);
+        lp.setVisible(true);
     }
 }
---
+++ src/main/java/com/myproject/carrentalsystem/signupPage.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.carrentalsystem;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.*;
+
+/**
+ *
+ * @author hicru
+ */
+public class signupPage extends JFrame implements ActionListener{
+    
+    private JLabel lblHeader, lblUsername, lblPassword;
+    private JButton btnLogin, btnReset, btnSignup;
+    private JTextField txtUsername, txtPassword;
+    
+    private String username = "admin";
+    private String password = "admin123";
+    protected static final ArrayList<String> screenSizes = new ArrayList<>(){{
+        add("Small screen");
+        add("Medium screen");
+        add("Normal screen");
+        }};
+
+    signupPage() {
+        this("Normal screen"); //Default
+    }
+    signupPage(String screenType) {
+        setName("Car Rental");
+        
+        if (screenType.equals("Small screen")) {
+        setSize(400, 600);
+        } else if (screenType.equals("Medium screen")) {
+        setSize(600, 700);
+        } else {
+        setSize(700, 600);
+        }
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        lblHeader = new JLabel("Car Rental App");
+        lblHeader.setBounds(50, 50, 100, 100);
+        add(lblHeader);
+        
+        lblUsername = new JLabel("Enter new Username: ", SwingConstants.RIGHT);
+        lblUsername.setBounds(100, 200, 200, 40);
+        add(lblUsername);
+        
+        lblPassword = new JLabel("Enter new Password: ", SwingConstants.RIGHT);
+        lblPassword.setBounds(100, 260, 200, 40);
+        add(lblPassword);
+        
+        txtUsername = new JTextField("Username");
+        txtUsername.setBounds(350, 200, 200, 40);
+        add(txtUsername);
+        
+        txtPassword = new JTextField("********");
+        txtPassword.setBounds(350, 260, 200, 40);
+        add(txtPassword);
+        
+        btnLogin = new JButton("LogIn");        
+        btnLogin.setBounds(230, 330, 80, 40);
+        add(btnLogin);
+        
+        btnReset = new JButton("Reset");        
+        btnReset.setBounds(400, 330, 80, 40);
+        add(btnReset);
+        
+        btnSignup = new JButton("Already have an account?");        
+        btnSignup.setBounds(200, 400, 310, 40);
+        add(btnSignup);
+        
+        btnLogin.addActionListener(this);
+        btnReset.addActionListener(this);
+        btnSignup.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnLogin) {
+            dispose();
+            loginPage lp = new loginPage();
+            lp.setVisible(true);
+        } else if (e.getSource() == btnReset) {
+            txtUsername.setText("");
+            txtPassword.setText("");
+        } else if (e.getSource() == btnSignup) {
+            dispose();
+            loginPage lp = new loginPage();
+            lp.setVisible(true);
+        }
+    }
+}
